d91014f54afb565274edcc5c1a52491b8c757820
Andy Truong <thehongtt@gmail.com>
2020-08-26 07:42:41 +1000

    Let the cow says.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
+extern crate ferris_says;
+
+use ferris_says::say as raw_say;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn say(s: &str) -> String {
-  println!("The Rust function say() received {}", s);
-  let r = String::from("hello ");
-  return r + s;
+    let mut vec = Vec::new();
+    raw_say(s.as_bytes(), 24, &mut vec).unwrap();
+
+    return String::from_utf8(vec).expect("Fount invalid UTF-8");
 }
